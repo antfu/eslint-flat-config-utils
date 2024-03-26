0c6ac82897c97a4421288b73fb8219f62fb1819e
Anthony Fu <anthonyfu117@hotmail.com>
2024-03-26 14:33:10 +0100

    chore: init

---
+++ src/index.ts
@@ -0,0 +1,88 @@
+import type { Linter } from 'eslint'
+
+export interface FlatConfigItem extends Linter.FlatConfig {
+  name?: string
+}
+
+export type Awaitable<T> = T | Promise<T>
+
+/**
+ * Concat multiple flat configs into a single flat config array.
+ *
+ * It also resolves promises and flattens the result.
+ */
+export async function concat(...configs: Awaitable<FlatConfigItem | FlatConfigItem[]>[]): Promise<FlatConfigItem[]> {
+  const resolved = await Promise.all(configs)
+  return resolved.flat()
+}
+
+/**
+ * A function that returns the config as-is, useful for providing type hints.
+ */
+export function defineFlatConfig(config: FlatConfigItem): FlatConfigItem {
+  return config
+}
+
+/**
+ * Rename plugin prefixes in a rule object.
+ * Accepts a map of prefixes to rename.
+ *
+ * @example
+ * ```ts
+ * import { renamePluginsInRules } from 'eslint-flat-config-utils'
+ *
+ * export default [{
+ *   rules: renamePluginsInRules(
+ *     {
+ *       '@typescript-eslint/indent': 'error'
+ *     },
+ *     { '@typescript-eslint': 'ts' }
+ *   )
+ * }]
+ * ```
+ */
+export function renamePluginsInRules(rules: Record<string, any>, map: Record<string, string>) {
+  return Object.fromEntries(
+    Object.entries(rules)
+      .map(([key, value]) => {
+        for (const [from, to] of Object.entries(map)) {
+          if (key.startsWith(`${from}/`))
+            return [to + key.slice(from.length), value]
+        }
+        return [key, value]
+      }),
+  )
+}
+
+/**
+ * Rename plugin names a flat configs array
+ *
+ * @example
+ * ```ts
+ * import { renamePluginsInConfigs } from 'eslint-flat-config-utils'
+ * import someConfigs from './some-configs'
+ *
+ * export default renamePluginsInConfigs(someConfigs, {
+ *   '@typescript-eslint': 'ts',
+ *   'import-x': 'import',
+ * })
+ * ```
+ */
+export function renamePluginsInConfigs(configs: FlatConfigItem[], map: Record<string, string>): FlatConfigItem[] {
+  return configs.map((i) => {
+    const clone = { ...i }
+    if (clone.rules)
+      clone.rules = renamePluginsInRules(clone.rules, map)
+    if (clone.plugins) {
+      clone.plugins = Object.fromEntries(
+        Object.entries(clone.plugins)
+          .map(([key, value]) => {
+            if (key in map)
+              return [map[key], value]
+            return [key, value]
+          }),
+      )
+    }
+    return clone
+  })
+}
